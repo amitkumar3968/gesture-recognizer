578c1533643e6a30831cb7cc596ed928d5d758e5
razic <razic@viralkitty.com>
2013-11-13 07:31:57 -0800

    GestureRecognizer#addTarget

--- index.js
+++ index.js
@@ -9,11 +9,32 @@ module.exports = GestureRecognizer;
 function GestureRecognizer() {}
 
 /**
+ * Private variables
+ */
+var targetActionPairs = [];
+
+/**
  * Prototype
  */
-GestureRecognizer.prototype = {
-  state: 'possible',
-  gestureRecognizerShouldBegin: function () {
-    return true;
+var prototype = Object.create(null);
+
+GestureRecognizer.prototype = prototype;
+
+prototype.addTarget = function (target, action, testTargetActionPairs) {
+  if (!target) {
+    throw new Error("You must specify a target");
+  } else if (!action) {
+    throw new Error("You must specify an action");
+  } else if (typeof action !== "string") {
+    throw new Error("Action must be a string");
+  } else if (typeof target[action] !== "function") {
+    throw new Error("The specified action must be a function of the target");
   }
+
+  var targets = (testTargetActionPairs || targetActionPairs);
+
+  for (var i = 0, l = targets.length; i < l; i ++)
+    if (targets[i][0] === target && targets[i][1] === action ) return;
+
+  targets.push([target, action]);
 };
--- test/gesture-recognizer.js
+++ test/gesture-recognizer.js
@@ -4,15 +4,88 @@ var gestureRecognizer = new GestureRecognizer(document);
 
 describe('GestureRecognizer', function () {
   describe('.prototype', function () {
-    describe('.state', function () {
-      it('should default to `possible`', function () {
-        assert(GestureRecognizer.prototype.state === 'possible');
-      });
-    });
+    describe('.addTarget(target, action)', function () {
+      var target, action, testTargetActionPairs;
 
-    describe('.gestureRecognizerShouldBegin', function () {
-      it('should default to `true`', function () {
-        assert(GestureRecognizer.prototype.gestureRecognizerShouldBegin() === true);
+      beforeEach(function() {
+        target = { myAction: function () {} };
+        action = 'myAction';
+        testTargetActionPairs = [];
+      });
+
+      it('should store the target-action pair', function () {
+        GestureRecognizer.prototype.addTarget(target, action, testTargetActionPairs);
+
+        assert(testTargetActionPairs[0][0] === target);
+        assert(testTargetActionPairs[0][1] === action);
+      });
+
+      describe(
+        'when attempting to add a target-action pair that has already been added',
+        function () {
+          it('should ignore the request', function () {
+            GestureRecognizer.prototype.addTarget(target, action, testTargetActionPairs);
+            GestureRecognizer.prototype.addTarget(target, action, testTargetActionPairs);
+
+            assert(testTargetActionPairs.length === 1);
+          });
+        }
+      );
+
+      describe('when `target` is falsey', function () {
+        it('should raise an error', function () {
+          var errorMessage;
+
+          try {
+            GestureRecognizer.prototype.addTarget();
+          } catch (error) {
+            errorMessage = error.message;
+          } finally {
+            assert(errorMessage === 'You must specify a target');
+          }
+        });
+      });
+
+      describe('when `action` is falsey', function () {
+        it('should raise an error', function () {
+          var errorMessage;
+
+          try {
+            GestureRecognizer.prototype.addTarget({});
+          } catch (error) {
+            errorMessage = error.message;
+          } finally {
+            assert(errorMessage === 'You must specify an action');
+          }
+        });
+      });
+
+      describe('when `action` is not a string', function () {
+        it('should raise an error', function () {
+          var errorMessage;
+
+          try {
+            GestureRecognizer.prototype.addTarget({}, {});
+          } catch (error) {
+            errorMessage = error.message;
+          } finally {
+            assert(errorMessage === 'Action must be a string');
+          }
+        });
+      });
+
+      describe('when the `action` is not a function of `target`', function () {
+        it('should raise an error', function () {
+          var errorMessage;
+
+          try {
+            GestureRecognizer.prototype.addTarget({}, 'foo');
+          } catch (error) {
+            errorMessage = error.message;
+          } finally {
+            assert(errorMessage === 'The specified action must be a function of the target');
+          }
+        });
       });
     });
   });
